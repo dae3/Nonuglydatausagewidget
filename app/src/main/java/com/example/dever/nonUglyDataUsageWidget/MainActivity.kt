4aa5ea03438664abae3c309ddf5a06136d56a6bd
Daniel Everett <deverett@gmail.com>
2018-01-20 20:33:46 +1100

    Correct pref keys

--- app/src/main/java/com/example/dever/nonUglyDataUsageWidget/MainActivity.kt
+++ app/src/main/java/com/example/dever/nonUglyDataUsageWidget/MainActivity.kt
@@ -1,46 +1,57 @@
 package com.example.dever.nonUglyDataUsageWidget
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.BitmapFactory
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import java.text.NumberFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mTextData : TextView
+    private lateinit var txtDataUsed: TextView
+    private lateinit var txtInterval: TextView
     private var nf = NumberFormat.getNumberInstance()
+//    private lateinit var stateInterval: NetworkStatsInterval
+    private lateinit var prefs : SharedPreferences
+//    private var myResources = getResources()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mTextData = findViewById(R.id.txtDataUsed)
+        txtDataUsed = findViewById(R.id.txtDataUsed)
+        txtInterval = findViewById(R.id.txtInterval)
+        prefs = getSharedPreferences(this.packageName, MODE_PRIVATE)
     }
 
+    @SuppressLint("ResourceType")
     override fun onResume() {
         super.onResume()
 
-        if (PermissionChecker.haveUsagePermission(this) && PermissionChecker.havePhoneStatePermission(this)) {
-            var d : Float = GetNetworkStats(DayNOfMonthNetworkStatsInterval(GregorianCalendar(), 1),this).getNetworkStats().toFloat()
-            mTextData.text = "${nf.format(d / 1024 / 1024)} MB"
-        } else {
+        // test permissions first - can't do much without these
+        if (!(PermissionChecker.haveUsagePermission(this) || !PermissionChecker.havePhoneStatePermission(this)))
             startActivity(Intent(this, PrePermissionRequestActivity::class.java))
+        else {
+            val dayOfMonth = prefs.getInt(
+                    resources.getString(R.string.prefs_key_billingcycle_startday), // TODO should these be R.string or R.id?
+                    1
+            )
+            Log.d(this.packageName, "dom is $dayOfMonth")
+            var stateInterval = DayNOfMonthNetworkStatsInterval(
+                    today = GregorianCalendar(),
+                    dayOfMonth = dayOfMonth
+            )
+
+            txtDataUsed.text = "${nf.format(GetNetworkStats(stateInterval, this).getNetworkStats().toFloat() / 1024 / 1024)} MB"
+            txtInterval.text = "$stateInterval"
         }
-
-        var iv = findViewById<ImageView>(R.id.imageView2)
-        iv.setImageBitmap(try {
-            PieWithTickChart(100, 100).bitmap
-        } catch (e : IllegalArgumentException) {
-            BitmapFactory.decodeResource(resources, R.drawable.example_appwidget_preview)
-        })
-
     }
 
-    fun onButtonClick(@Suppress("UNUSED_PARAMETER") v : View) {
+    fun onButtonClick(@Suppress("UNUSED_PARAMETER") v: View) {
         startActivity(Intent(this, PreferencesActivity::class.java))
     }
 
