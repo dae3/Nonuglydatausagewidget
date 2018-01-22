d396ee124516c968277f520481875b3350331e67
Daniel Everett <deverett@gmail.com>
2018-01-23 06:51:33 +1100

    Max and actual data gets all moved to GetNetworkStats

---
+++ app/src/main/java/com/example/dever/nonUglyDataUsageWidget/NetworkStatsIntervalFactory.kt
@@ -0,0 +1,22 @@
+package com.example.dever.nonUglyDataUsageWidget
+
+import android.content.Context
+import android.preference.PreferenceManager
+import java.util.*
+
+/**
+ * Factory class for NetworkStatsInterval
+ */
+object NetworkStatsIntervalFactory {
+
+    fun getInterval(context : Context) : NetworkStatsInterval {
+        // for now just the one
+        return DayNOfMonthNetworkStatsInterval(
+                GregorianCalendar(),
+                PreferenceManager.getDefaultSharedPreferences(context).getString(
+                        context.resources.getString(R.string.prefs_key_billingcycle_startday),
+                        "99"
+                ).toInt()
+        )
+    }
+}
