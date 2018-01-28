1f36a38c671b326c1d5495a73796436d09ad818c
Daniel Everett <deverett@gmail.com>
2018-01-28 20:36:37 +1100

    Add ActionBar to main activity

--- app/src/main/java/com/example/dever/nonUglyDataUsageWidget/MainActivity.kt
+++ app/src/main/java/com/example/dever/nonUglyDataUsageWidget/MainActivity.kt
@@ -1,37 +1,56 @@
 package com.example.dever.nonUglyDataUsageWidget
 
-import android.app.Activity
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.view.View
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import java.text.NumberFormat
 import java.util.*
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var txtDataUsed: TextView
     private lateinit var txtInterval: TextView
     private lateinit var i: ImageView
     private var nf = NumberFormat.getNumberInstance()
-    //    private lateinit var stateInterval: NetworkStatsInterval
     private lateinit var prefs: SharedPreferences
     private lateinit var stats: GetNetworkStats
     private lateinit var chart: PieWithTickChart
-//    private var myResources = getResources()
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this, PreferencesActivity::class.java))
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<android.support.v7.widget.Toolbar>(R.id.mainactivty_toolbar)
+        setSupportActionBar(toolbar)
+
         txtDataUsed = findViewById(R.id.txtDataUsed)
         txtInterval = findViewById(R.id.txtInterval)
+
         i = findViewById(R.id.imageView2)
         prefs = PreferenceManager.getDefaultSharedPreferences(this)
-//        stats = GetNetworkStats(this, )
         chart = PieWithTickChart(100, 100, this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar, menu)
+        return super.onPrepareOptionsMenu(menu)
     }
 
     override fun onResume() {
@@ -58,9 +77,4 @@ class MainActivity : Activity() {
             }
         }
     }
-
-    fun onButtonClick(@Suppress("UNUSED_PARAMETER") v: View) {
-        startActivity(Intent(this, PreferencesActivity::class.java))
-    }
-
 }
