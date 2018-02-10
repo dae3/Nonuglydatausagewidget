8f34f68f095a07bdefbf3dbf20342631e3f591cb
Daniel Everett <deverett@gmail.com>
2018-02-10 17:45:02 +1100

    PermissionChecker no longer launches PrePermissionRequestActivity

--- app/src/main/java/com/example/dever/nonUglyDataUsageWidget/GetNetworkStats.kt
+++ app/src/main/java/com/example/dever/nonUglyDataUsageWidget/GetNetworkStats.kt
@@ -3,28 +3,19 @@ package com.example.dever.nonUglyDataUsageWidget
 import android.annotation.SuppressLint
 import android.app.usage.NetworkStatsManager
 import android.content.Context
-import android.content.Intent
 import android.net.ConnectivityManager.TYPE_MOBILE
 import android.preference.PreferenceManager
 import android.telephony.TelephonyManager
 
+@SuppressLint("MissingPermission", "HardwareIds")
+@SuppressWarnings("MissingPermission") // callers will catch the Exception
 class GetNetworkStats(private val context: Context, private var interval: NetworkStatsInterval) {
 
-    private var nsm: NetworkStatsManager = context.getSystemService(NetworkStatsManager::class.java)
-    private var subscriberId: String
+    private var nsm = context.getSystemService(NetworkStatsManager::class.java)
 
-    init {
-        if (!PermissionChecker.havePhoneStatePermission(context)) context.startActivity(Intent(context, PrePermissionRequestActivity::class.java))
-
-        // courtesy of https://medium.com/@quiro91/build-a-data-usage-manager-in-android-e7991cfe7fe4
-        val tm = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        try {
-            @SuppressLint("HardwareIds") // subscriberID is required to call querySummaryForDevice later
-            subscriberId = tm.subscriberId
-        } catch (e: SecurityException) {
-            // TODO do something with exception: back to PrePermissionRequestActivitity?
-            subscriberId = ""
-        }
+    // get subscriber id code courtesy of https://medium.com/@quiro91/build-a-data-usage-manager-in-android-e7991cfe7fe4
+    private val subscriberId: String by lazy {
+        (context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager).subscriberId
     }
 
     var actualData: Long = 0L
