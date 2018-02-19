19ffaac22f136ee4be764eddafc65d4abe085c65
Daniel Everett <deverett@gmail.com>
2018-02-20 06:42:35 +1100

    Refactored PieChartFragment to use Kotlin synthetic view properties

--- app/src/main/java/com/example/dever/nonUglyDataUsageWidget/PieChartFragment.kt
+++ app/src/main/java/com/example/dever/nonUglyDataUsageWidget/PieChartFragment.kt
@@ -8,9 +8,7 @@ import android.view.View
 import android.view.View.*
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import kotlinx.android.synthetic.main.pie_chart.*
 import kotlin.math.roundToInt
 
 /**
@@ -22,19 +20,11 @@ class PieChartFragment : Fragment(), ViewTreeObserver.OnGlobalLayoutListener {
     private lateinit var statsInterval: NetworkStatsInterval
     private lateinit var networkStats: GetNetworkStats
     private lateinit var pie: PieWithTickChart
-    private lateinit var img: ImageView
-    private lateinit var txtData: TextView
-    private lateinit var txtDays: TextView
-    private lateinit var imgError : ImageView
     private lateinit var rootView: View
     private var layoutDone = false
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         rootView = inflater?.inflate(R.layout.pie_chart, container, false)!!
-        img = rootView.findViewById(R.id.widgetChartImageView)
-        txtData = rootView.findViewById(R.id.txtWidgetActualData)
-        txtDays = rootView.findViewById(R.id.txtWidgetDays)
-        imgError = rootView.findViewById(R.id.widgetErrorImageView)
 
         statsInterval = NetworkStatsIntervalFactory.getInterval(activity)
         networkStats = GetNetworkStats(activity, statsInterval)
@@ -55,30 +45,30 @@ class PieChartFragment : Fragment(), ViewTreeObserver.OnGlobalLayoutListener {
         rootView.viewTreeObserver.removeOnGlobalLayoutListener(this)
         if (!layoutDone) {
             layoutDone = true
-            rootView.findViewById<ProgressBar>(R.id.progressBar).visibility = GONE
+            progressBar.visibility = GONE
         }
 
         val density = resources.displayMetrics.density
-        pie = PieWithTickChart(context, rootView.width/density.roundToInt(), rootView.height/density.roundToInt(), statsInterval, networkStats)
+        pie = PieWithTickChart(context, rootView.width / density.roundToInt(), rootView.height / density.roundToInt(), statsInterval, networkStats)
 
         try {
-            img.setImageBitmap(pie.bitmap)
-            txtData.text = pie.actualDataText
+            widgetChartImageView.setImageBitmap(pie.bitmap)
+            txtWidgetActualData.text = pie.actualDataText
             // this doesn't make sense since we gave the PieWithTickChart constructor dimensions
             // in DP, but hey, it works
-            txtData.setTextSize(COMPLEX_UNIT_PX, pie.actualDataTextSize)
-            txtDays.text = pie.daysText
-            txtDays.setTextSize(COMPLEX_UNIT_PX, pie.daysTextSize)
+            txtWidgetActualData.setTextSize(COMPLEX_UNIT_PX, pie.actualDataTextSize)
+            txtWidgetDays.text = pie.daysText
+            txtWidgetDays.setTextSize(COMPLEX_UNIT_PX, pie.daysTextSize)
 
-            img.visibility = VISIBLE
-            txtData.visibility = VISIBLE
-            txtDays.visibility = VISIBLE
-            imgError.visibility = INVISIBLE
+            widgetChartImageView.visibility = VISIBLE
+            txtWidgetActualData.visibility = VISIBLE
+            txtWidgetDays.visibility = VISIBLE
+            widgetErrorImageView.visibility = INVISIBLE
         } catch (e: SecurityException) {
-            img.visibility = INVISIBLE
-            txtData.visibility = INVISIBLE
-            txtDays.visibility = INVISIBLE
-            imgError.visibility = VISIBLE
+            widgetChartImageView.visibility = INVISIBLE
+            txtWidgetActualData.visibility = INVISIBLE
+            txtWidgetDays.visibility = INVISIBLE
+            widgetErrorImageView.visibility = VISIBLE
         }
     }
 }
