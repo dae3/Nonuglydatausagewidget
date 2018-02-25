2420650428417ded9c7b07fa567d5ec12e1cbf0e
Daniel Everett <deverett@gmail.com>
2018-02-26 08:59:02 +1100

    Sync off laptop

--- app/src/test/java/com/example/dever/nonUglyDataUsageWidget/FirstRunPreferenceCaptureActivityTest.kt
+++ app/src/test/java/com/example/dever/nonUglyDataUsageWidget/FirstRunPreferenceCaptureActivityTest.kt
@@ -1,13 +1,19 @@
 package com.example.dever.nonUglyDataUsageWidget
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.test.suitebuilder.annotation.SmallTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.eq
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
 import org.robolectric.RobolectricTestRunner
-import org.mockito
+
 
 /**
  * Unit tests for FirstRunPreferenceActivity
@@ -18,12 +24,16 @@ class FirstRunPreferenceCaptureActivityTest {
 
 //    @get:Rule
 //    val rule = ActivityTestRule(FirstRunPreferenceCaptureActivity::class.java, )
-        var mockMain: MainActivity = Mockito.
+        var mockMain: mockActivity = Mockito.mock(mockActivity::class.java)
+        private val requestCode = 0
 
 
     @Before
     fun setUp() {
-        mockMain = Robolectric.setupActivity(MainActivity::class.java)
+        `when`(mockMain.onActivityResult(eq(requestCode), ArgumentMatchers.anyInt(), ArgumentMatchers.any(Intent::class.java)))
+                .someVoid
+
+
 
         // no need to do anything else - Activity has no knowledge of 1st run state, that's
         // MainActivity's problem
@@ -33,8 +43,19 @@ class FirstRunPreferenceCaptureActivityTest {
     fun returnsSuccessCodeIfContinuePressed() {
         mockMain.startActivityForResult(
                 Intent(mockMain, FirstRunPreferenceCaptureActivity::class.java),
-                0
+                requestCode
         )
+
+        verify(
+                mockMain.onActivityResult(eq(requestCode), eq(RESULT_OK), ArgumentMatchers.any(Intent::class.java))
+        )
+    }
+
+    // jank to get around protected visibility in base class
+    open inner class mockActivity : Activity() {
+        public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 
 }
