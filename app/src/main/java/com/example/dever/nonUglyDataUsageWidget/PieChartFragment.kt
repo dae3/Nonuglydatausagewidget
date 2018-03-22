97ad6baeb5df40ac435e63e26a1a79173f7941ab
Daniel Everett <deverett@gmail.com>
2018-03-23 06:29:31 +1100

    Generates AppWidget preview image
    Added build variant AndroidManifest.xml for debug and dummyData types
    Click on "day N of M" text saves current view as PNG (in dummyData type)

--- app/src/main/java/com/example/dever/nonUglyDataUsageWidget/PieChartFragment.kt
+++ app/src/main/java/com/example/dever/nonUglyDataUsageWidget/PieChartFragment.kt
@@ -1,21 +1,28 @@
 package com.example.dever.nonUglyDataUsageWidget
 
 import android.app.Fragment
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
+import android.os.Environment
 import android.util.TypedValue.COMPLEX_UNIT_PX
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.*
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
+import android.widget.Toast
 import kotlinx.android.synthetic.main.pie_chart.*
+import java.io.File
+import java.io.FileOutputStream
 import kotlin.math.roundToInt
 
 /**
  * Subclass of fragment to display network usage stats in main activity (and any other non-
  * widget uses in the future
  */
-class PieChartFragment : Fragment(), ViewTreeObserver.OnGlobalLayoutListener {
+class PieChartFragment : Fragment(), ViewTreeObserver.OnGlobalLayoutListener, OnClickListener {
 
     private lateinit var statsInterval: NetworkStatsInterval
     private lateinit var networkStats: GetNetworkStats
@@ -64,11 +71,34 @@ class PieChartFragment : Fragment(), ViewTreeObserver.OnGlobalLayoutListener {
             txtWidgetActualData.visibility = VISIBLE
             txtWidgetDays.visibility = VISIBLE
             widgetErrorImageView.visibility = INVISIBLE
+
+            txtWidgetDays.setOnClickListener(this)
         } catch (e: SecurityException) {
             widgetChartImageView.visibility = INVISIBLE
             txtWidgetActualData.visibility = INVISIBLE
             txtWidgetDays.visibility = INVISIBLE
             widgetErrorImageView.visibility = VISIBLE
+        }
+    }
+
+    /**
+     * For build purposes only, save a PNG of the current view, for use as an AppWidgetPreview image
+     * Also needs WRITE_EXTERNAL_STORAGE permission declared in manifest
+     */
+    override fun onClick(v: View?) {
+        if (BuildConfig.BUILD_TYPE == "dummyData" && v?.id == R.id.txtWidgetDays) {
+            var b = Bitmap.createBitmap(view.measuredWidth,view.measuredHeight,Bitmap.Config.ARGB_4444)
+            rootView.draw(Canvas(b))
+
+
+            if (context.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PERMISSION_GRANTED)
+                activity.requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),0)
+            else {
+                val os = FileOutputStream(File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "appwidget_preview.png"))
+                val ok = b.compress(Bitmap.CompressFormat.PNG, 0, os)
+                os.close()
+                Toast.makeText(view.context, "AppWidget preview ${if (!ok) "not " else ""}saved", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
